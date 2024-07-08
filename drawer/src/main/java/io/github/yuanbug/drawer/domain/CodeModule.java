2a5aa32394c13ac2e0f885143413b38520ebb0e0
yuanbug <59193722+yuanbug@users.noreply.github.com>
2024-07-08 11:53:45 +0800

    代码优化

--- drawer/src/main/java/io/github/yuanbug/drawer/domain/CodeModule.java
+++ drawer/src/main/java/io/github/yuanbug/drawer/domain/CodeModule.java
@@ -1,19 +1,18 @@
 package io.github.yuanbug.drawer.domain;
 
 import io.github.yuanbug.drawer.parser.maven.DependencyJarFile;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import java.nio.file.Path;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author yuanbug
  */
 @Data
-@Builder
-@AllArgsConstructor
 public class CodeModule {
 
     public final String name;
@@ -21,5 +20,12 @@ public class CodeModule {
     public final Path srcMainJavaPath;
 
     public final List<DependencyJarFile> compileDependencyJars;
+
+    @Builder
+    public CodeModule(String name, Path srcMainJavaPath, List<DependencyJarFile> compileDependencyJars) {
+        this.name = Objects.requireNonNull(name);
+        this.srcMainJavaPath = Objects.requireNonNull(srcMainJavaPath);
+        this.compileDependencyJars = Objects.requireNonNullElseGet(compileDependencyJars, Collections::emptyList);
+    }
 
 }
